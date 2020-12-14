0b96de7e1fb464428a36980102fb65a18b56870d
ahmet <ahmet.beylihan@gmail.com>
2020-12-14 22:06:10 +0300

    first init

---
+++ main.py
@@ -0,0 +1,39 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import cv2
+from deepface import DeepFace
+
+def start_analyzing_mimic():
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    cap = cv2.VideoCapture(1)
+
+    if not cap.isOpened():
+        cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        raise IOError("No camera detected")
+
+    while True:
+        ret, frame = cap.read()
+        result_analyzer = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        print(result_analyzer['dominant_emotion'])
+        # start to persist into database all emotions
+
+        cv2.putText(frame, result_analyzer['dominant_emotion'], (50, 50), font, 3, (0, 0, 255), 2, cv2.LINE_4)
+        cv2.imshow('Original video', frame)
+
+        if cv2.waitKey(2) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+if __name__ == '__main__':
+    start_analyzing_mimic()
